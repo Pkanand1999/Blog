c74685d2f126639c5e221a29d413fad4d05efdd4
Pkanand1999 <singhisking810267@gmail.com>
2023-06-04 22:41:43 +0530

    socket created

--- server/server.js
+++ server/server.js
@@ -8,7 +8,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:true}))
 app.use(cors())
 
-io.on('connection',(Socket)=>{});
+io.on('connection',(Socket)=>{
+    Socket.on('join-room',(data)=>{
+        const {roomId,emailId} = data;
+        Socket.join(roomId);
+        Socket.broadcast.to(roomId).email("user-joined",{emailId});
+    })
+});
 
 app.listen(8080,()=>{
     console.log('listening on port 8080');
